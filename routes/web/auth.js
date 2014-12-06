00c5c16d8e7583561a5504ebd59f2a2801975b38
Jorge Izquierdo <jorge@izqui.me>
2014-12-06 21:36:41 +0100

    get auth 2

--- routes/web/auth.js
+++ routes/web/auth.js
@@ -4,43 +4,53 @@ var Token = require('mongoose').model('Token')
 var getAuth = function(req, res) {
 
 	var permissions = JSON.parse(req.query.permissions || "[\"name\"]"),
-		provider = req.query.provider
+		provider = req.query.provider,
+		callback = req.query.callback
 
-	Token.findOne({provider: provider}, function (err, token) {
+	if (!provider || !callback) {
 
-		var already = false
+		res.status(500).send("Bad formed request. Lacking provider or callback")
+	} else {
 
-		if (token) {
-			already = true
-		} 
-		// [["name", 0], ["email", 1]]
-		async.reduce(permissions, [], function (vs, p, cb){
+		Token.findOne({provider: provider}, function (err, token) {
 
-			var found = 0
+			var already = false
+
 			if (token) {
-
-				token.permissions.forEach(function (pi){
-					found = (pi == p)
-				})
+				already = true
 			} 
+			// [["name", 0], ["email", 1]]
+			async.reduce(permissions, [], function (vs, p, cb){
 
-			vs.push([p, found])
-			cb(null, vs)
-			
-		}, function(err, permissions){
-			if (err) {
-				res.sendStatus(500)
-			} else {
+				var found = 0
+				if (token) {
 
-				res.render('auth', {provider: 'Twitter', permissions: permissions, alreadyAuthorized: already})
-			}
+					token.permissions.forEach(function (pi){
+						found = (pi == p)
+					})
+				} 
+				//TODO: Not push data not existent in database
+				vs.push([p, found])
+				cb(null, vs)
+
+			}, function(err, permissions){
+				if (err) {
+					res.sendStatus(500)
+				} else {
+
+					res.render('auth', {provider: provider, permissions: permissions, alreadyAuthorized: already, callback: callback})
+				}
+			})
 		})
-	})
+	}
 }
 
 var postAuth = function(req, res) {
 
-	var accepted = req.body.accepted
+	var provider = req.body.provider,
+		callback = req.body.callback
+
+
 
 }
 

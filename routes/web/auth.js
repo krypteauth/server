d34cd423efcbcc72c4bc90bb42362e3be077a8d7
Jorge Izquierdo <jorge@izqui.me>
2014-12-06 21:29:56 +0100

    get auth

--- routes/web/auth.js
+++ routes/web/auth.js
@@ -1,13 +1,46 @@
+var async = require('async')
+var Token = require('mongoose').model('Token')
 
 var getAuth = function(req, res) {
 
-	var permissions = JSON.parse(req.query.permissions || "[\"name\"]")
+	var permissions = JSON.parse(req.query.permissions || "[\"name\"]"),
+		provider = req.query.provider
 
-	res.render('auth', {provider: 'Twitter', permissions: permissions})
+	Token.findOne({provider: provider}, function (err, token) {
+
+		var already = false
+
+		if (token) {
+			already = true
+		} 
+		// [["name", 0], ["email", 1]]
+		async.reduce(permissions, [], function (vs, p, cb){
+
+			var found = 0
+			if (token) {
+
+				token.permissions.forEach(function (pi){
+					found = (pi == p)
+				})
+			} 
+
+			vs.push([p, found])
+			cb(null, vs)
+			
+		}, function(err, permissions){
+			if (err) {
+				res.sendStatus(500)
+			} else {
+
+				res.render('auth', {provider: 'Twitter', permissions: permissions, alreadyAuthorized: already})
+			}
+		})
+	})
 }
 
 var postAuth = function(req, res) {
 
+	var accepted = req.body.accepted
 
 }
 

b145b405650a9963bfaf51bebe3f082198bacb1a
Jorge Izquierdo <jorge@izqui.me>
2014-12-07 04:01:18 +0100

    docker

--- db.js
+++ db.js
@@ -5,7 +5,7 @@ var config = require('./config');
 
 module.exports = exports = function() {
 
-	mongoose.connect(config.db);
+	mongoose.connect(process.env.MONGO_PORT_27017_TCP_ADDR || config.db);
 	
 	for (var model in models) {
 		mongoose.model(model, models[model])
